3d608f67d1483a061a4bc391831e89541d69be7b
qlccnoibo <khanhbuifb.media@gmail.com>
2026-08-29 01:20:56 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3613,7 +3613,7 @@ function loadPersonalRecords() {
       var eatBadge = recItem.eat === 'Có' ? 'yes' : 'no',
         shiftName = recItem.shift ? recItem.shift.split('(')[0].trim() : '-';
       var escapedNote = noteStr.replace(/'/g, "\\'").replace(/"/g, '&quot;').replace(/\n/g, ' ');
-      html += '<tr><td class="date-compact">' + formatDate(recItem.date) + '</td><td><span class="shift-badge">' + shiftName + '</span></td><td class="task-list-compact">' + tasksStr + '</td><td>' + colleaguesHtml + '</td><td><span class="eat-badge ' + eatBadge + '">' + (recItem.eat || '-') + '</span></td><td class="note-cell-personal" onclick="showNotePopup(event, \'' + escapedNote + '\')" onmouseenter="showNotePopup(event, \'' + escapedNote + '\')" onmouseleave="hideNotePopup()">' + noteDisplay + '</td></tr>';
+      html += '<tr><td class="date-compact">' + formatDate(recItem.date) + '</td><td><span class="shift-badge ' + getShiftColorClass(shiftName) + '">' + shiftName + '</span></td><td class="task-list-compact">' + tasksStr + '</td><td>' + colleaguesHtml + '</td><td><span class="eat-badge ' + eatBadge + '">' + (recItem.eat || '-') + '</span></td><td class="note-cell-personal" onclick="showNotePopup(event, \'' + escapedNote + '\')" onmouseenter="showNotePopup(event, \'' + escapedNote + '\')" onmouseleave="hideNotePopup()">' + noteDisplay + '</td></tr>';
     });
     html += '</table></div></div>';
     recordsEl.innerHTML = html;
